69fdd12abcf924bd1f007c60626fea79cc93a20f
Sirupsen <sirup@sirupsen.dk>
2010-05-03 18:25:58 +0200

    Structure

---
+++ models/user.rb
@@ -0,0 +1,10 @@
+require "mongo_mapper"
+
+MongoMapper.connection = Mongo::Connection.new
+MongoMapper.database = 'mydb'
+
+class User
+  include MongoMapper::Document
+
+  key :username, String, :required => true
+end
