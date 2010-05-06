fdb77947ce6d32643e718d6279071a7eec44a8a0
Sirupsen <sirup@sirupsen.dk>
2010-05-06 15:36:10 +0200

    Comments
    Added some kick ass comments.

--- mongo.rb
+++ mongo.rb
@@ -1,11 +1,13 @@
+# Few comments because Ruby sort of comments itself with it's elegance
+
 %w{
   sinatra
   erb
   models/user.rb
-}.each {|lib| require lib}
+}.each {|lib| require lib} # Because DRY code, is awesome code
 
 configure do
-  set :haml, {:format => :html5}
+  set :haml, {:format => :html5} # Because HTML5 is what the cool kids use
 end
 
 get "/" do
